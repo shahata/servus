ad7043b7bc5e51426a0bb436586a128e75cef328
Shahar Talmi <shahar.talmi@gmail.com>
2013-07-02 00:03:00 +0300

    added plugin syntax to config

--- servus.conf.js
+++ servus.conf.js
@@ -1,18 +1,15 @@
+'use strict';
+
+var server = 'localhost';
+
 module.exports = {
   port: 80,
   gzip: true,
   cors: true,
 
-  variables: {
-    server: 'localhost'
-  },
-
   aliases: {
     '/': '../../Downloads',
-    '/docs': ['../../Downloads', '../../Documents'],
-    '/docs/dl': 'http://localhost/docs'
-    //'/docs/dl': 'http://{{server}}',
-
-    //'/blob': {plugin: 'blob', params: {}}
+    '/docs/dl': 'http://' + server + '/docs',
+    '/docs': ['../../Downloads', '../../Documents']
   }
 };
